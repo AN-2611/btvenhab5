fb6c8eae42e0dadb786ebf871a91c9634e0989e7
AN-2611 <Thai26112003@gmail.com>
2024-12-20 07:40:32 +0700

    Add project files.

---
+++ btb5.1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using btb5._1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+
+namespace btb5._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Trang chủ
+        public IActionResult Index()
+        {
+            // Kiểm tra session
+            var username = HttpContext.Session.GetString("Username");
+            if (string.IsNullOrEmpty(username))
+            {
+                // Chuyển về trang Login nếu chưa đăng nhập
+                return RedirectToAction("Login", "Account");
+            }
+
+            ViewBag.Username = username;
+            return View();
+        }
+
+        // Trang Privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Xử lý lỗi
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
